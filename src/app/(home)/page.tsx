ca4dcf8c4a4007feaef5017ea9491fe08d5ad5bf
npmh310 <ngphminhhieu310@gmail.com>
2026-02-12 14:20:47 +0700

    update prerender

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,8 @@
 import { getProjects } from "@/lib/projects";
 import { HomeContent } from "./components/HomeContent";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const projects = await getProjects();
   return <HomeContent projects={projects} />;
